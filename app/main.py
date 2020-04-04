8644d96ae5ec2fd8a2bdabde3763a406202678aa
seba <sebastian@wierzchow.ski>
2020-04-04 20:01:11 +0200

    a

--- app/main.py
+++ app/main.py
@@ -1,32 +1,47 @@
 import uuid
 
 import aioboto3
+import boto3
 from fastapi import FastAPI
 
 app = FastAPI()
-
-
-@app.get("/dynamodb")
-async def dynamo_get(param=None):
-    async with aioboto3.resource("dynamodb", region_name='eu-central-1') as dynamo_resource:
-        table = await dynamo_resource.Table('random_uuid')
-        result = await table.get_item(Key={'uuid': param or str(uuid.uuid4())})
-        return result.get('Item', 'Not found')
-
-
-@app.post("/dynamodb")
-async def dynamo_post():
-    async with aioboto3.resource("dynamodb", region_name='eu-central-1') as dynamo_resource:
-        table = await dynamo_resource.Table('random_uuid')
-        result = await table.put_item(Item={'uuid': str(uuid.uuid4())})
-        return result['Item']
+# dynamodb_synch = boto3.resource('dynamodb', region_name='eu-central-1')
+# dynamodb_asynch = await aioboto3.resource('dynamodb', region_name='eu-central-1')
+#
+#
+# @app.get("/dynamodb-synch")
+# def dynamo_get_synch(param=None):
+#     table = dynamodb_synch.Table('random_uuid')
+#     result = table.get_item(Key={'uuid': param or str(uuid.uuid4())})
+#     return result.get('Item', 'Not found')
+#
+#
+# @app.post("/dynamodb-synch")
+# def dynamo_post():
+#     table = dynamodb_synch.Table('random_uuid')
+#     result = table.put_item(Item={'uuid': str(uuid.uuid4())})
+#     return result['Item']
+#
+#
+# @app.get("/dynamodb-asynch")
+# async def dynamo_get(param=None):
+#     table = await dynamodb_asynch.Table('random_uuid')
+#     result = await table.get_item(Key={'uuid': param or str(uuid.uuid4())})
+#     return result.get('Item', 'Not found')
+#
+#
+# @app.post("/dynamodb-asynch")
+# async def dynamo_post():
+#     table = await dynamodb_asynch.Table('random_uuid')
+#     result = await table.put_item(Item={'uuid': str(uuid.uuid4())})
+#     return result['Item']
 
 
 @app.get("/healthcheck")
-async def asdf():
+def healthcheck():
     return "OK"
 
 
 @app.get("/")
-async def asdf():
+async def root():
     return "OK"
